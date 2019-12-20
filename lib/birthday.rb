b4a82a977ec16bff42fedd156215d596d4cb4be2
Lydia B <lydiabaik@gmail.com>
2019-12-20 00:53:49 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -17,7 +17,8 @@ def age_appropriate_birthday(birthday_kids)
     if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old! You are too old for this.""
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!
+      You are too old for this."
 
     end
 end
